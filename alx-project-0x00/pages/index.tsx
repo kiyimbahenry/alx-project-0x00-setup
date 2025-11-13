675c7048f68cc0dfb8c5a34c8b05581345b802a3
kiyimba_henry <kiyimbahenry314@gmail.com>
2025-11-13 12:36:00 +0300

    Rendering Components

--- alx-project-0x00/pages/index.tsx
+++ alx-project-0x00/pages/index.tsx
@@ -1,12 +1,15 @@
+import Card from "@/components/Card"
+
 const Home: React.FC = () => {
     return (
-        <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
-            <h1>Airbnb Application Clone system</h1>
-            <button className="border px-3 py-1 text-lg mt-3 bg-blue-500 text-white rounded-full">
+        <main className="flex flex-col items-center p-8">
+            <h1 className="text-4xl font-semibold mb-8">Airbnb Application Clone system</h1>
+            <button className="border px-3 py-1 text-lg mt-3 bg-blue-500 text-white rounded-full mb-8">
                 Get Started
             </button>
+            <Card />
         </main>
     )
 }
 
-export default Home;
+export default Home
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,8 +1,12 @@
+import Card from "@/components/Card"
+
 const Landing: React.FC = () => {
     return (
-        <div>
-            <h1 className="text-xl font-extralight">Landing Page</h1>
+        <div className="p-8">
+            <h1 className="text-xl font-extralight mb-4">Landing Page</h1>
+            <Card />
         </div>
     )
 }
+
 export default Landing
